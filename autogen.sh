1c5e49f893dad2857c0570e4545f7bd51268ec5e
Florian Stinglmayr <florian@n0la.org>
2018-02-21 10:32:03 +0100

    add autogen and build scripts for convenience

---
+++ autogen.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+set -e
+set -x
+
+cd "$(dirname "$0")"
+
+PATH=$PATH:/usr/local/bin
+
+aclocal $AC_SEARCH_OPTS
+autoconf
+autoheader
+automake --add-missing
---
+++ build.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+set -e
+set -x
+
+cd "$(dirname "$0")"
+
+./autogen.sh
+./configure "$@"
+make -j4
